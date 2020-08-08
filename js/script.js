ac7c3ad3c7572e835cd1ad8ca466298ad7aef387
Emerson21983 <55989868+Emerson21983@users.noreply.github.com>
2020-08-08 17:22:54 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,3 +22,40 @@ var pieChart = new Chart(grafica, {
   type: 'pie',
   data: Data
 });
+
+//Botones navbar
+var quees = document.getElementById("quees");
+var ventajas = document.getElementById("ventajas");
+var instalacion = document.getElementById("instalacion");
+
+//Contenido a mostrar y ocultar
+var contenidoquees = document.getElementById("contenidoquees");
+var contenidoventajas = document.getElementById("contenidoventajas");
+var contenidoinstalacion = document.getElementById("contenidoinstalacion");
+
+quees.onclick = function(){
+    contenidoquees.classList.remove("ocultar");
+    contenidoquees.classList.add("mostrar");
+    contenidoventajas.classList.remove("mostrar");
+    contenidoventajas.classList.add("ocultar");
+    contenidoinstalacion.classList.remove("mostrar");
+    contenidoinstalacion.classList.add("ocultar");
+}
+
+ventajas.onclick = function(){
+    contenidoventajas.classList.remove("ocultar");
+    contenidoventajas.classList.add("mostrar");
+    contenidoquees.classList.remove("mostrar");
+    contenidoquees.classList.add("ocultar");
+    contenidoinstalacion.classList.remove("mostrar");
+    contenidoinstalacion.classList.add("ocultar");
+}
+
+instalacion.onclick = function(){
+    contenidoinstalacion.classList.remove("ocultar");
+    contenidoinstalacion.classList.add("mostrar");
+    contenidoventajas.classList.remove("mostrar");
+    contenidoventajas.classList.add("ocultar");
+    contenidoquees.classList.remove("mostrar");
+    contenidoquees.classList.add("ocultar");
+}
