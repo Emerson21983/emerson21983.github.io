8e89e6748578b1a3c7e5d8ccf32a405267b7fa41
Emerson21983 <55989868+Emerson21983@users.noreply.github.com>
2021-02-25 23:58:04 -0600

    Add files via upload

--- tallerEje2/js/agregar.js
+++ tallerEje2/js/agregar.js
@@ -1,1 +1,33 @@
+//input
+var inputEsfera = document.getElementById("esfera");
+var inputCubo = document.getElementById("cubo");
+var inputCilindro = document.getElementById("cilindro");
 
+//Botones enviar
+var enviarEsfera = document.getElementById("enviarEsfera");
+var enviarCubo = document.getElementById("enviarCubo");
+var enviarCilindro = document.getElementById("enviarCilindro");
+
+enviarEsfera.onclick = function(){
+    if(inputEsfera.value == ""){
+        alert("Ingrese la medida");
+    }else{
+        document.formEsfera.submit();
+    }
+}
+
+enviarCubo.onclick = function(){
+    if(inputCubo.value == ""){
+        alert("Ingrese la medida");
+    }else{
+        document.formCubo.submit();
+    }
+}
+
+enviarCilindro.onclick = function(){
+    if(inputCilindro.value == ""){
+        alert("Ingrese la medida");
+    }else{
+        document.formCilindro.submit();
+    }
+}
